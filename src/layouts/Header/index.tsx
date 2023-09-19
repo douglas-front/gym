303ad4a32f0943b47cba2dfdc9fc091666ee7e20
douglas-front <douglasjknw007@gmail.com>
2023-09-19 16:54:01 -0300

    adicionado seção de produtos

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -7,14 +7,15 @@ export function Header() {
       <header>
         <div className="container-header">
           
-          <div className='bg'></div>
           
           <div className='text-header'>
 
             <h1>Classic suplementos</h1>
 
-            <p>confira nossa infinita possibilidades de suplementos</p>
-            <p>promoções , preços baixos para você!</p>
+            <div className="p-text">
+              <p>confira nossa infinita possibilidades de suplementos</p>
+              <p>promoções , preços baixos para você!</p>
+            </div>
 
             <div className='div-button-header'>
               <button>Saiba mais!</button>
@@ -22,6 +23,7 @@ export function Header() {
 
           </div>
 
+          <div className='bg'></div>
         </div>
       </header>
     );
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,17 @@
 import { Nav } from "../../components/nav"
 import { WhatsIcon } from "../../components/whatsapp-Icon"
 import { Header } from "../../layouts/Header"
+import { Produtos } from "../../layouts/produtos"
 
 export function Home (){
   return(
     <>
       <Nav/>
-      <Header/>
-      <WhatsIcon/>
+      <main>
+        <Header/>
+        <WhatsIcon/>
+        <Produtos/>
+      </main>
     </>
   )
 }
